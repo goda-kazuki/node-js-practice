2ab6cfc3c151496cdb55c0ea4f1c9915db1cc9b5
合田和樹 <goda@eyemovic.com>
2020-01-09 16:28:22 +0900

    削除機能も実装

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const multer = require("multer");
+const uuidv4=require("uuid/v4");
 
+const app = express();
+
+app.use(multer().none());
 app.use(express.static("web"));
 
-var listener = app.listen(3000, () => console.log(listener.address().port));
-
-app.get('/', function (req, res) {
-    res.send('ページが表示されました！');
-});
+const todoList=[{title:"タイトル",id:"1234",done: true}];
 
 app.get("/api/v1/list",function (req,res) {
-    const todoList=[
-        {title:"javascriptの勉強",done:true},
-        {title:"Node.jsを勉強",done:true},
-        {title:"Web APIを作る",done:false},
-    ];
     res.json(todoList);
 });
+
+app.post("/api/v1/add",function (req,res) {
+    const todoData=req.body;
+    const todoTitle=todoData.title;
+
+    const id=uuidv4();
+
+    const todoItem={
+        id,
+        title:todoTitle,
+        done:false
+    };
+
+    todoList.push(todoItem);
+
+    console.log("Add: "+ JSON.stringify(todoItem));
+
+    res.json(todoItem);
+});
+
+app.delete("/api/v1/item/:id",function (req,res) {
+    const index=todoList.findIndex(function (item) {
+        return item.id===req.params.id;
+    });
+
+
+    if(index>=0){
+        const deleted=todoList.splice(index,1);
+        console.log("Delete:"+JSON.stringify(deleted[0]));
+    }
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("サーバー立ち"));
