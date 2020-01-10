ecb4d406e6ea2982f20406591d76f0e6b6d63b18
合田和樹 <goda@eyemovic.com>
2020-01-10 11:14:09 +0900

    チェックボックスの反映(更新機能)を実装

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
 const express = require('express');
 const multer = require("multer");
-const uuidv4=require("uuid/v4");
+const uuidv4 = require("uuid/v4");
 
 const app = express();
 
 app.use(multer().none());
 app.use(express.static("web"));
 
-const todoList=[{title:"タイトル",id:"1234",done: true}];
+const todoList = [{title: "タイトル", id: "1234", done: false}];
 
-app.get("/api/v1/list",function (req,res) {
-    res.json(todoList);
+app.get("/api/v1/list", function (req, res) {
+  res.json(todoList);
 });
 
-app.post("/api/v1/add",function (req,res) {
-    const todoData=req.body;
-    const todoTitle=todoData.title;
+app.post("/api/v1/add", function (req, res) {
+  const todoData = req.body;
+  const todoTitle = todoData.title;
 
-    const id=uuidv4();
+  const id = uuidv4();
 
-    const todoItem={
-        id,
-        title:todoTitle,
-        done:false
-    };
+  const todoItem = {
+    id,
+    title: todoTitle,
+    done: false
+  };
 
-    todoList.push(todoItem);
+  todoList.push(todoItem);
 
-    console.log("Add: "+ JSON.stringify(todoItem));
+  console.log("Add: " + JSON.stringify(todoItem));
 
-    res.json(todoItem);
+  res.json(todoItem);
 });
 
-app.delete("/api/v1/item/:id",function (req,res) {
-    const index=todoList.findIndex(function (item) {
-        return item.id===req.params.id;
-    });
+app.delete("/api/v1/item/:id", function (req, res) {
+  const index = todoList.findIndex(function (item) {
+    return item.id === req.params.id;
+  });
 
+  if (index >= 0) {
+    const deleted = todoList.splice(index, 1);
+    console.log("Delete:" + JSON.stringify(deleted[0]));
+  }
+  res.sendStatus(200);
+});
 
-    if(index>=0){
-        const deleted=todoList.splice(index,1);
-        console.log("Delete:"+JSON.stringify(deleted[0]));
-    }
-    res.sendStatus(200);
+app.put("/api/v1/item/:id", function (req, res) {
+
+  const index = todoList.findIndex(function (item) {
+
+    return item.id === req.params.id;
+  });
+
+  if (index >= 0) {
+    const item = todoList[index];
+
+    item.done = req.body.done === "true";
+    console.log("PUT:" + JSON.stringify(item))
+  }
+
+  res.sendStatus(200);
+
 });
 
 app.listen(3000, () => console.log("サーバー立ち"));
