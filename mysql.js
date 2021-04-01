7779ff9dfe881b923c7a7c8b2f349d59b263864b
合田和樹 <goda@eyemovic.com>
2021-04-01 17:15:06 +0900

    変更

--- mysql.js
+++ mysql.js
@@ -5,7 +5,7 @@ mysqlConfig.connection.connect();
 mysqlConfig.connection.query("select * from todos;", function (err, rows, fields) {
   if (err) throw err;
 
-  console.log(rows);
+  console.log("sample");
 });
 
 mysqlConfig.connection.end();
